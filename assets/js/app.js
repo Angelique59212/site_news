54ac2a2e2d28476a3ac64827cf3ea2775034e79e
angelika <dehainaut.angelique@orange.fr>
2022-11-23 10:28:28 +0100

    ==> responsive SASS

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ import {Api} from "./Api";
 import {Dark} from "./Dark";
 import {Article} from "./Article";
 
-import ('../styles/style.scss');
+import '../styles/style.scss';
 
 const requestUrl = 'https://newsapi.org/v2/top-headlines?country=fr&apiKey=0bbc52e16fa6451e8c3354b59dd06f98';
 
